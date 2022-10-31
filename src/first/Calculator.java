b9da7cbd7c42b5879548a9fe00d66641f105309d
ibraeva@technokratos.com <ibraeva@technokratos.com>
2022-10-31 13:57:32 +0300

    add recursion for continue

--- src/first/Calculator.java
+++ src/first/Calculator.java
@@ -5,6 +5,32 @@ import java.util.Scanner;
 public class Calculator {
     Scanner scanner = new Scanner(System.in);
 
+    //основные методы в одном
+    void makeCalculatorGreatAgain(){
+        printText();
+        chooseOperation();
+    }
+
+    //спрашиваем пользователя о продолжении
+    public void askForContinue(){
+        System.out.println("Желаете продолжить? [y/n]: ");
+        if (scanner.hasNext()){
+            String answer = scanner.next();
+
+            switch (answer) {
+                case "y":
+                    makeCalculatorGreatAgain();
+                    break;
+                case "n":
+                    break;
+                default:
+                    System.out.println("Введите одно из перечисленных значений (y/n)");
+                    askForContinue();
+            }
+        }
+        else System.out.println("первый if не сработал((");
+    }
+
     //текст
     void printText(){
         System.out.println("                       Калькулятор               ");
@@ -44,15 +70,19 @@ public class Calculator {
             switch (scanner.nextInt()){
                 case 1:
                     System.out.println("Сумма: " + sum(enterNumber(), enterNumber()));
+                    askForContinue();
                     break;
                 case 2:
                     System.out.println("Разница: " + sub(enterNumber(), enterNumber()));
+                    askForContinue();
                     break;
                 case 3:
                     System.out.println("Произведение: " + mult(enterNumber(), enterNumber()));
+                    askForContinue();
                     break;
                 case 4:
                     System.out.println("Частное: " + div(enterNumber(), enterNumber()));
+                    askForContinue();
                     break;
                 default:
                     System.out.println("Выберите одно из значений, перечисленных выше.");
--- src/first/Main.java
+++ src/first/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
 
         Calculator c = new Calculator();
-        c.printText();
-        c.chooseOperation();
+        c.makeCalculatorGreatAgain();
+
     }
 }
